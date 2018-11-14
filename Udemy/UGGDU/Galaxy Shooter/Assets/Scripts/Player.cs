1c3b0a29282433907cf0f709dcc56ef0212be215
Mathew Welch <gmwva@hotmail.com>
2018-11-13 20:43:22 -0500

    Player Contraints and Code Clean-up

--- Udemy/UGGDU/Galaxy Shooter/Assets/Scripts/Player.cs
+++ Udemy/UGGDU/Galaxy Shooter/Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
     public float speed = 5.0f;
 
 	// Use this for initialization
@@ -15,10 +16,33 @@ public class Player : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
+        Movement();
+    }
+
+    private void Movement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
         transform.Translate(Vector3.up * speed * verticalInput * Time.deltaTime);
+
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y < -4.2f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.2f, 0);
+        }
+
+        if (transform.position.x > 9.5f)
+        {
+            transform.position = new Vector3(-9.5f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -9.5f)
+        {
+            transform.position = new Vector3(9.5f, transform.position.y, 0);
+        }
     }
 }
