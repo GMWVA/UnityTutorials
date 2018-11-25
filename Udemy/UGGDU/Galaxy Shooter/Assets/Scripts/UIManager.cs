d70ece2df7a412298b32adefc16d7b13de3c9332
Mathew Welch <gmwva@hotmail.com>
2018-11-24 20:16:01 -0500

    Started UI

---
+++ Udemy/UGGDU/Galaxy Shooter/Assets/Scripts/UIManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public Sprite[] lives;
+
+    public void UpdateLives()
+    {
+        
+    }
+
+    public void UpdateScore()
+    {
+
+    }
+}
